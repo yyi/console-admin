24fec55377e544dbe79f4f30e7a49483fe47d22a
yyi <yyi.mailer@gmail.com>
2020-01-16 14:04:44 +0800

    优化office文档生成

--- console-web-commons/src/main/java/com/founder/console/web/utils/OfficeDocumentGeneratorUtil.java
+++ console-web-commons/src/main/java/com/founder/console/web/utils/OfficeDocumentGeneratorUtil.java
@@ -4,7 +4,6 @@ import com.founder.Exception.OperationException;
 import com.founder.exception.sysadmin.SysadminError;
 import com.github.liaochong.myexcel.core.HtmlToExcelFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.poi.poifs.filesystem.DirectoryEntry;
@@ -18,7 +17,9 @@ import org.thymeleaf.context.Context;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import javax.annotation.Resource;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.Map;
@@ -34,8 +35,8 @@ public class OfficeDocumentGeneratorUtil {
     }
 
     @FunctionalInterface
-    interface OfficeDocumentGenerator {
-        void apply(String t, OutputStream u) throws Exception;
+    interface OfficeDocumentGenerator<T extends OutputStream> {
+        void apply(String t, T u) throws Exception;
     }
 
     private final TemplateEngine templateEngine;
@@ -60,13 +61,18 @@ public class OfficeDocumentGeneratorUtil {
         }
     }
 
-    public byte[] createWordDoc(String templateName, Map map, String pdfFilePath) throws Exception {
+    public byte[] createWordDoc(String templateName, Map map, String pdfFilePath) {
         return createOfficeDocument(templateName, map, this::createWordDocument);
 
     }
 
     public byte[] createExcel(String templateName, Map map, String pdfFilePath) {
         return createOfficeDocument(templateName, map, this::createExcelDocument);
+    }
+
+
+    public byte[] createPdf(String templateName, Map map, String pdfFilePath) {
+        return createOfficeDocument(templateName, map, this::createPdfDocument);
     }
 
     private void createWordDocument(String html, OutputStream bos) throws Exception {
@@ -81,31 +87,15 @@ public class OfficeDocumentGeneratorUtil {
         workbook.write(bos);
     }
 
-
-
-    public byte[] createPdf(String templateName, Map map, String pdfFilePath) {
-        Assert.notNull(templateName, "The templateName can not be null");
-        String processedHtml = getHtml(templateName, map);
+    private void createPdfDocument(String html, OutputStream bos) throws Exception {
         ITextRenderer renderer = null;
         try {
-            final File outputFile = new File(pdfFilePath);
-
-            if (outputFile.exists()) {
-                throw new IOException("文件已存在！");
-            }
-
-            ByteArrayOutputStream bos = new ByteArrayOutputStream(100 * 1024);
-
-//            ITextRenderer renderer = new ITextRenderer();
-//            ITextFontResolver fontResolver = renderer.getFontResolver();
-//
-//            fontResolver.addFont(sunFontPath, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
             StopWatch stopWatch = new StopWatch();
             if (log.isDebugEnabled())
                 stopWatch.start("初始化");
             renderer = pdfTextRendererObjectPool.borrowObject();
 
-            renderer.setDocumentFromString(processedHtml);
+            renderer.setDocumentFromString(html);
             renderer.layout();
             if (log.isDebugEnabled()) {
                 stopWatch.stop();
@@ -117,19 +107,12 @@ public class OfficeDocumentGeneratorUtil {
                 stopWatch.stop();
                 log.debug(stopWatch.prettyPrint());
             }
-            log.debug("PDF created successfully");
-            byte[] data = bos.toByteArray();
-            FileUtils.writeByteArrayToFile(outputFile, data);
-            return data;
-        } catch (Throwable e) {
-            log.error("pdf文件生成失败", e);
-            throw new OperationException(SysadminError.PdfFileGenerateError, e);
         } finally {
             if (!Objects.isNull(renderer))
                 pdfTextRendererObjectPool.returnObject(renderer);
-
         }
     }
+
 
     private String getHtml(String templateName, Map map) {
         Context ctx = new Context();
